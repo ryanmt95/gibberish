ea0892395fa08f85022843cd8b3f7b0651103904
KerrynEer <39527245+KerrynEer@users.noreply.github.com>
2020-07-07 23:09:50 +0800

    Create modal for user to enter nickname

---
+++ gibberish_client/src/components/Joinroom/NicknameModal.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function NicknameModal( { handleClose, isShowingModal, action }) {
+    const showHideClassName = isShowingModal ? "modal display-block" : "modal display-none"
+    return (
+        <div className={showHideClassName} role="dialog" aria-hidden="true">
+            <div className="modal-dialog modal-dialog-centered" role="document">
+                <div className="modal-content text-center">
+                    <div className="modal-body">
+                        <button type="button" className="close" aria-label="Close" onClick={handleClose}>
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        <div id="nicknameTitle">Please enter a nickname</div>
+                        <br />
+                        <div className="input-group w-75 mx-auto">
+                            <input type="text" className="form-control text-center rounded-pill" placeholder="Nickname"/>
+                        </div>
+                        <br />
+                        <button type="button" className="btn btn-success w-25" id="joinRoomButton">OK</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default NicknameModal

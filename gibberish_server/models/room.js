011f93789e19fb01b2b7da38d47a2c1241b240ec
Cicilia Helena <hwcicilia@gmail.com>
2020-07-11 14:05:52 +0800

    add state to room model

--- gibberish_server/models/room.js
+++ gibberish_server/models/room.js
@@ -1,9 +1,13 @@
 import uid from 'uid';
 
 
+const STATE = {"stop":"STOPING", "play":"PLAYING", "pause":"PAUSING"}
+Object.freeze(STATE)
+
 class Room {
     constructor() {
         this.id = uid();
+        this.state = STATE.stop
         this.round = 0;
         this.players = [];
         this.remainingTime = 0;
@@ -12,11 +16,14 @@ class Room {
     startRound() {
         this.round++
         this.remainingTime = 10;
+        this.state = STATE.PLAYING
         setTimeout(function run() {
             this.remainingTime--;
             console.log(this.remainingTime);
             if(this.remainingTime > 0) {
                 setTimeout(run, 1000);
+            } else {
+                this.state = STATE.pause
             }
         }, 1000);
     }

5859c28b77b5d6b96f55b50ce37d45592e2744bf
Ryan <ryan.m.tan95@gmail.com>
2020-07-12 18:15:30 +0800

    remove create questions endpoint

--- gibberish_server/routes.js
+++ gibberish_server/routes.js
@@ -2,7 +2,6 @@ const QuestionsController = require('./controllers/questions_controller')
 const RoomController = require('./controllers/room_controller')
 
 module.exports = (app) => {
-    app.get('/create_questions', QuestionsController.createQuestions);
     app.get('/question', QuestionsController.getRandomQuestion);
     app.get('/room/:roomId', RoomController.getRoomData);
     app.post('/create_room', RoomController.createNewRoom);

b8eecc5fc08eed07640545c22770d505b053e147
Cicilia Helena <hwcicilia@gmail.com>
2020-07-17 00:54:07 +0800

    create test for updateScore player model

--- gibberish_server/test/models_test/player_test.js
+++ gibberish_server/test/models_test/player_test.js
@@ -5,7 +5,7 @@ const player1 = new Player("Ryan");
 const player2 = new Player("Ryan", 1300, 705);
 const jsonOfPlayer2 = {
     playerName: "Ryan",
-    totalScore: 1300,
+    totalScore: "1300",
     lastScore: 705
 };
 
@@ -14,6 +14,14 @@ describe('Test of player model', function () {
     describe('toJSON()', function () {
         it('should return the correct json of a player', function () {
             assert.deepEqual(player2.toJSON(), jsonOfPlayer2);
+        });
+    });
+
+    describe('updateScore(newScore)', function () {
+        it('should return the correct score value', function () {
+            player1.updateScore(595)
+            player1.updateScore(705)
+            assert.deepEqual(player1, player2);
         });
     });
 });

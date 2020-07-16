f6676ed1eca0f8847596902e523fcac9fc08eb7e
Cicilia Helena <hwcicilia@gmail.com>
2020-07-17 00:42:53 +0800

    create test for toJSON player model

---
+++ gibberish_server/test/models_test/player_test.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+const Player = require("../../models/player")
+
+const player1 = new Player("Ryan");
+const player2 = new Player("Ryan", 1300, 705);
+const jsonOfPlayer2 = {
+    playerName: "Ryan",
+    totalScore: 1300,
+    lastScore: 705
+};
+
+
+describe('Test of player model', function () {
+    describe('toJSON()', function () {
+        it('should return the correct json of a player', function () {
+            assert.deepEqual(player2.toJSON(), jsonOfPlayer2);
+        });
+    });
+});

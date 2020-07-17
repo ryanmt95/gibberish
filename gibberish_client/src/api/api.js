5be22c28bc681a83de58193e1326046a1e10f5f5
kev-inc <kevinchanxy@gmail.com>
2020-07-18 00:43:46 +0800

    changed baseURL to obtain from env variables

--- gibberish_client/src/api/api.js
+++ gibberish_client/src/api/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: process.env.BASEURL || 'http://localhost:4000'
+  baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost:4000'
 });

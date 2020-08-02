62f4c1f3ae2b6cb97a9cc32813353bf6da5e8649
KerrynEer <39527245+KerrynEer@users.noreply.github.com>
2020-08-02 15:11:54 +0800

    Add hints and few more questions

--- gibberish_server/sample_questions.js
+++ gibberish_server/sample_questions.js
@@ -1,101 +1,154 @@
 const questions = [
     {
         question: "poe day tote ships",
-        answer: "potato chips"
+        answer: "potato chips",
+        hint: "A snack made of potatoes!"
     },
     {
         question: "jik hin ugh its",
-        answer: "chicken nuggets"
+        answer: "chicken nuggets",
+        hint: "McDonalds sells this!"
     },
     {
         question: "fur rose in yoke hurt",
-        answer: "frozen yogurt"
+        answer: "frozen yogurt",
+        hint: "Beware of a brainfreeze!"
     },
     {
         question: "road ibra tuh",
-        answer: "roti prata"
+        answer: "roti prata",
+        hint: "It's flat and round, usually eaten with curry!"
     },
     {
         question: "chi can rise",
-        answer: "chicken rice"
+        answer: "chicken rice",
+        hint: "Local's favourite!"
     },
     {
         question: "bar pearl tee",
-        answer: "bubble tea"
+        answer: "bubble tea",
+        hint: "Comes with chewy pearls!"
     },
     {
         question: "mech spy sea",
-        answer: "mcspicy"
+        answer: "mcspicy",
+        hint: "It's a little spicy!"
     },
     {
         question: "chi lick rab",
-        answer: "chilli crab"
+        answer: "chilli crab",
+        hint: "Singapore's number 1!"
     },
     {
         question: "fee shed curl ree",
-        answer: "fish head curry"
+        answer: "fish head curry",
+        hint: "Be careful of the bones in it!"
     },
     {
         question: "nas sill erm mak",
-        answer: "nasi lemak"
+        answer: "nasi lemak",
+        hint: " A malay cuisine dish!"
     },
     {
         question: "some all stink gray",
-        answer: "sambal stingray"
+        answer: "sambal stingray",
+        hint: "A type of seafood!"
     },
     {
         question: "mirr you boose",
-        answer: "mee rebus"
+        answer: "mee rebus",
+        hint: "A type of Malay noodles!"
     },
     {
         question: "charred way tell",
-        answer: "char kway teow"
+        answer: "char kway teow",
+        hint: "Unhealthy but amazing!"
     },
     {
         question: "car reap huff",
-        answer: "curry puff"
+        answer: "curry puff",
+        hint: "Old Chang Kee's famous for this!"
     },
     {
         question: "car rick she can nu dells",
-        answer: "curry chicken noodles"
+        answer: "curry chicken noodles",
+        hint: "3 words!"
     },
     {
         question: "mail odine no saw",
-        answer: "milo dinosaur"
+        answer: "milo dinosaur",
+        hint: "Comes with cocoa powder!"
     },
     {
         question: "miso toe",
-        answer: "mee soto"
+        answer: "mee soto",
+        hint: "A noodle soup dish"
     },
     {
         question: "bend and kick",
-        answer: "pandan cake"
+        answer: "pandan cake",
+        hint: "It's soft, fluffy and green!"
     },
     {
         question: "tay tar rick",
-        answer: "teh tarik"
+        answer: "teh tarik",
+        hint: "A type of tea!"
     },
     {
         question: "eyes lea monty",
-        answer: "iced lemon tea"
+        answer: "iced lemon tea",
+        hint: "Refreshing and sweet!"
     },
     {
         question: "can ducky fry eyed chi can",
-        answer: "kentucky fried chicken"
+        answer: "kentucky fried chicken",
+        hint: "Finger Lickin' Good!"
     },
     {
         question: "ber girk ying",
-        answer: "burger king"
+        answer: "burger king",
+        hint: "Has onion rings!"
     },
     {
         question: "peace are hard",
-        answer: "pizza hut"
+        answer: "pizza hut",
+        hint: "A flat, pie shape food!"
     },
     {
         question: "shh ache shh aack",
-        answer: "shake shack"
+        answer: "shake shack",
+        hint: "Did someone say burgers and milkshake?"
+    },
+    {
+        question: "sa bu ay",
+        answer: "subway",
+        hint: "A healthy to-go!"
+    },
+    {
+        question: "one ton me",
+        answer: "wanton mee",
+        hint: "Popular in Hong Kong!"
+    },
+    {
+        question: "bar ret tork",
+        answer: "bread talk",
+        hint: "Famous for floss buns!"
+    },
+    {
+        question: "sad day",
+        answer: "satay",
+        hint: "Comes on a stick!"
+    },
+    {
+        question: "luck sar",
+        answer: "laksa",
+        hint: "White noodles in spicy soup!"
+    },
+    {
+        question: "carry odd kate",
+        answer: "carrot cake",
+        hint: "Can be black or white!"
     }
-    
 ]
 
 module.exports = questions;
